3c05a5f0d8424a115e7e2d889a0dbf5752cec585
Jz-powpow <113581514+Jz-powpow@users.noreply.github.com>
2024-01-04 21:48:06 -0300

    main.py

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Client(
     'bot',
-    api_id= API_ID, #get it from https://my.telegram.org/auth
-    api_hash="API_HASH", #get it from https://my.telegram.org/auth
-    bot_token="BOT_TOKEN", #get it from @Botfather
+    api_id= 26032794, #get it from https://my.telegram.org/auth
+    api_hash="1ccced9f1c971ee459e5c539792cb2f6", #get it from https://my.telegram.org/auth
+    bot_token="6973378210:AAG9WARDLIVfvmRZ8QQ6tsJdyUCvcGtyO1s", #get it from @Botfather
     plugins=dict(root="plugins"),
     parse_mode="html")
 
